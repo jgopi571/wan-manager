09ad04ffde1578032818f275a915798e5556fd93
JayajothiGopi <Jayajothi_Gopi@comcast.com>
2026-02-11 15:33:43 +0530

    Update wanmgr_network_monitor.c
    ipv6 deletion

--- source/WanManager/wanmgr_network_monitor.c
+++ source/WanManager/wanmgr_network_monitor.c
@@ -257,8 +257,8 @@ static ANSC_STATUS parse_addrattr(struct nlmsghdr *nlh)
        stAddrEvent.preferred_lft = pref_lifetime;
        stAddrEvent.valid_lft     = valid_lifetime;
 
-       CcspTraceInfo(("%s-%d [ADDR EVENT] RTM_NEWADDR (new/updated address) for '%s' interface, Info 'NEWADDR|%s|%u|%u|%u'\n", __FUNCTION__, __LINE__, ifname, ipv6_addr, prefix_length, pref_lifetime, valid_lifetime));
-       WanMgr_Handle_Dhcpv6_NetLink_Address_Event(&stAddrEvent);
+       CcspTraceInfo(("%s-%d jothi [ADDR EVENT] RTM_NEWADDR (new/updated address) for '%s' interface, Info 'NEWADDR|%s|%u|%u|%u'\n", __FUNCTION__, __LINE__, ifname, ipv6_addr, prefix_length, pref_lifetime, valid_lifetime));
+     //  WanMgr_Handle_Dhcpv6_NetLink_Address_Event(&stAddrEvent);
        ret = ANSC_STATUS_SUCCESS;
     } 
     else if (nlh->nlmsg_type == RTM_DELADDR) 
@@ -271,8 +271,8 @@ static ANSC_STATUS parse_addrattr(struct nlmsghdr *nlh)
        stAddrEvent.preferred_lft = pref_lifetime;
        stAddrEvent.valid_lft     = valid_lifetime;
 
-       CcspTraceInfo(("%s-%d [ADDR EVENT] RTM_DELADDR (address removed/expired) for '%s' interface, Info 'DELADDR|%s|%u|%u|%u'\n", __FUNCTION__, __LINE__, ifname, ipv6_addr, prefix_length, pref_lifetime, valid_lifetime));
-       WanMgr_Handle_Dhcpv6_NetLink_Address_Event(&stAddrEvent);
+       CcspTraceInfo(("%s-%d jothi [ADDR EVENT] RTM_DELADDR (address removed/expired) for '%s' interface, Info 'DELADDR|%s|%u|%u|%u'\n", __FUNCTION__, __LINE__, ifname, ipv6_addr, prefix_length, pref_lifetime, valid_lifetime));
+       //WanMgr_Handle_Dhcpv6_NetLink_Address_Event(&stAddrEvent);
        ret = ANSC_STATUS_SUCCESS;
     }
 
